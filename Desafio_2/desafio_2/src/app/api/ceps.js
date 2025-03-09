b5fdef91d93c95f02388d99dfb7664f2ea420f9c
Gabriel Raso Pagotti <gabrielpagotti007@gmail.com>
2025-03-09 15:15:39 -0300

    modified:   Desafio_2/desafio_2/src/app/api/ceps.js

--- Desafio_2/desafio_2/src/app/api/ceps.js
+++ Desafio_2/desafio_2/src/app/api/ceps.js
@@ -4,16 +4,17 @@ import { delete_row } from "./api"
 
 export function Ceps({ ceps }){
   const cep = use(ceps)
-  const css= `border-2 px-2 border-slate-100/20`
+  const css= `border-1 px-2 border-slate-100/20`
   try {
     return (cep.map(e => (
   
   
-    <tr id={e.id}>
-      <td className={css}>{e.cidade}</td>
-      <td className={css}>{e.bairro}</td>
-      <td className={css}>{e.estado}</td>
-      <button type="button" className="p-2 cursor-pointer " onClick={()=> delete_row(e.id, e.cep)}>X</button>
+    <tr key={e.id} id={e.id} >
+      <td  className={css}>{e.cidade}</td>
+      <td  className={css}>{e.bairro}</td>
+      <td  className={css}>{e.estado}</td>
+      <td  className={css + ` cursor-pointer text-center text-red-900/90`}><button type="button" 
+       onClick={()=> delete_row(e.id, e.cep)}>X</button></td>
   </tr>
   )))
   } catch (error) {
